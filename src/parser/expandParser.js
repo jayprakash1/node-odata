828101d4442b340ced1450a7c028bbd243eabf82
Jay Prakash <jay.prakash1@gmail.com>
2015-10-11 21:55:21 +0530

    first cut for filter in expand

--- src/parser/expandParser.js
+++ src/parser/expandParser.js
@@ -1,10 +1,32 @@
 "use strict";
 
+import Builder from 'sequelize-query-builder'
+import filterParser from './filterParser';
+
 // a small way to convert it to expected format internally
 export default (query, expand) => {
   if( expand == null )
     return;
 
-  const SPLIT_MULTIPLE_EXPANDS = ','  
-  query.include(expand.split(SPLIT_MULTIPLE_EXPANDS))
+  let expands;
+  let includes = [];
+  //TODO: '/' recursive include
+  const SPLIT_MULTIPLE_EXPANDS = ',';
+
+  expands = expand.split(SPLIT_MULTIPLE_EXPANDS);
+  let subQuery = new Builder();
+  for(let i=0; i < expands.length; i++){
+    let item = expands[i];
+    if(item.indexOf('$filter')){
+      let arr = item.match(/(.+?)\(\$filter=(.+?)\)/).map((s) => s.trim()).filter((n) => n);
+      if(arr.length !== 3){
+        return new Error("Syntax error at '#{item}'.");
+      }
+      filterParser(subQuery, arr[2]);
+      includes.push({model: arr[1], where: subQuery._where})
+    } else {
+      includes.push({model: item});
+    }
+  }
+  query.include(includes)
 };
